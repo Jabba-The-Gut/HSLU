024fce9fd38aa768cb0367cff5fd5e1d8cbbc264
daveeee <david.gut@iflow.ch>
2017-11-01 15:39:25 +0100

    Formatted Circle-Klass

--- oop_maven_template/src/main/java/ch/hslu/oop/sw06/Shape/Circle.java
+++ oop_maven_template/src/main/java/ch/hslu/oop/sw06/Shape/Circle.java
@@ -10,29 +10,29 @@ package ch.hslu.oop.sw06.Shape;
  *
  */
 public class Circle extends Shape {
-    private double diameter;
+	private double diameter;
 
-    public Circle(int x, int y, double diameter) {
-        super(x, y);
-        this.diameter = diameter;
-    }
+	public Circle(int x, int y, double diameter) {
+		super(x, y);
+		this.diameter = diameter;
+	}
 
-    public final void setDiameter(double diameter) {
-        this.diameter = diameter;
-    }
+	public final void setDiameter(double diameter) {
+		this.diameter = diameter;
+	}
 
-    public final double getDiameter() {
-        return diameter;
-    }
+	public final double getDiameter() {
+		return diameter;
+	}
 
-    @Override
-    public double getPerimeter() {
-        return diameter * Math.PI;
-    }
+	@Override
+	public double getPerimeter() {
+		return diameter * Math.PI;
+	}
 
-    @Override
-    public double getArea() {
-        return (Math.PI * Math.pow(diameter, 2));
-    }
+	@Override
+	public double getArea() {
+		return (Math.PI * Math.pow(diameter, 2));
+	}
 
 }
