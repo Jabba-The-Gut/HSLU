caa324211ed2387c05aa6ba7821f435406102e44
daveeee <david.gut@iflow.ch>
2017-11-08 21:48:59 +0100

    ...

--- oop_maven_template/src/main/java/ch/hslu/oop/sw08/Element/Main.java
+++ oop_maven_template/src/main/java/ch/hslu/oop/sw08/Element/Main.java
@@ -3,14 +3,13 @@ package ch.hslu.oop.sw08.Element;
 public class Main {
 
 	public static void main(String[] args) {
-		Blei elem = new Blei("Blei", 20);
+		Blei elem = new Blei("Blei", 2000);
 		Quecksilber elem2 = new Quecksilber("Quecksilber", 200);
 		Stickstoff elem3 = new Stickstoff("Stickstoff", -246);
 		
 		System.out.println(elem.toString());
 		System.out.println(elem2.toString());
 		System.out.println(elem3.toString());
-
 	}
 
 }
